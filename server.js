19ace46ee800f87849ad5ba0c2d47a11618de9af
root <root@DESKTOP-KGE7GI1.localdomain>
2021-05-18 11:53:37 +0700

    first commit

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const PORT = process.env.PORT || 8082
 
 
 app.post('/api', (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-    let eid = req.body.eid;
     return res.json({
         status: 'ok',
         data: {
-            'username' : username,
-            'password' : password,
-            'eid' : eid
+            'username' : 'aa',
+            'password' : 'aa',
+            'eid' : 'eid'
         }
     })
 });
