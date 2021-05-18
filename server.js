f78ac39bde507b6b8951d2571bc429af4ae29042
root <root@DESKTOP-KGE7GI1.localdomain>
2021-05-18 14:40:52 +0700

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+//app.use(express.urlencoded({extended: false}));
 const PORT = process.env.PORT || 8082
 
 app.post('/', (req, res) => {
-    console.log(req.body);
+    console.log(req.test);
     res.json({
         status: 'ok',
     })
